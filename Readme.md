cc94a638979dea240f2405728aafffddf95110e5
iitsuraj <suraj.saini.phe17@itbhu.ac.in>
2020-08-13 22:28:31 +0530

    test

---
+++ Readme.md
@@ -0,0 +1,5 @@
+config for DEVOPS
+
+1. chmode +x ./runme.sh
+2. Enter answers
+3. cheers!!
---
+++ runme.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+OUT=./git-hooks/pre-commit
+echo setting git hooks
+echo enter ftpserverip
+read ftpserver
+echo enter ftp username
+read ftpuser
+echo enter ftp user password
+read ftppassword
+mkdir git-hooks
+touch ./git-hooks/pre-commit
+cat <<EOF >$OUT
+    #!/bin/sh
+
+    echo making Release
+    cd samvaad-web-client
+    yarn build
+    7z a webapp webapp
+
+    ftp -inv $ftpserver << EOF
+    user $ftpuser $ftppassword
+    put webapp.7z
+    bye 
+    EOF
+    echo upload completed
+EOF
+chmod +x ./git-hooks/pre-commit
+ln -s ../../git-hooks/pre-commit .git/hooks
+
+echo git hook setting completed
