1efadc1fd23daa66b20390e658e22da444570a8f
iitsuraj <suraj.saini.phe17@itbhu.ac.in>
2020-08-13 22:38:05 +0530

    ignore githokks

--- runme.sh
+++ runme.sh
@@ -7,6 +7,7 @@ echo enter ftp username
 read ftpuser
 echo enter ftp user password
 read ftppassword
+sudo rm -rf git-hooks
 mkdir git-hooks
 touch ./git-hooks/pre-commit
 cat <<EOF >$OUT
@@ -25,6 +26,7 @@ cat <<EOF >$OUT
     echo upload completed
 EOF
 chmod +x ./git-hooks/pre-commit
+sudo rm -rf ./.git/hooks/pre-commit
 ln -s ../../git-hooks/pre-commit .git/hooks
 
 echo git hook setting completed
